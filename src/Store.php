2af24af53db277c2d74f4a8faab1c6841f8fe34d
Kevin Tokheim <katokh89@gmail.com>
2015-08-28 09:59:49 -0700

    add save, getAll and deleteAll methods and tests for Store class.

--- src/Store.php
+++ src/Store.php
@@ -28,6 +28,57 @@
         {
             return $this->id;
         }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO stores (store_name) VALUES (
+                '{$this->getStoreName()}'
+            );");
+
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        static function getAll()
+        {
+            $returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores;");
+            $stores = array();
+            foreach ($returned_stores as $store) {
+                $store_name = $store['store_name'];
+                $id = $store['id'];
+                $new_store = new Store($store_name, $id);
+                array_push($stores, $new_store);
+            }
+            return $stores;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stores;");
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -14,11 +14,11 @@
 
     class StoreTest extends PHPUnit_Framework_TestCase
     {
-        // protected function tearDown()
-        // {
-        //     Store::deleteAll();
-        //     Brand::deleteAll();
-        // }
+        protected function tearDown()
+        {
+            Store::deleteAll();
+
+        }
 
         function test_setStoreName()
         {
@@ -60,5 +60,84 @@
             //Assert
             $this->assertEquals(1, $result);
         }
+
+        function test_getAll()
+        {
+            //Arrange
+            $store_name = "Flying Shoes";
+            $store_name2 = "Magic Shoes";
+            $test_store = new Store($store_name);
+            $test_store->save();
+            $test_store2 = new Store($store_name2);
+            $test_store2->save();
+
+            //Act
+            $result = Store::getAll();
+
+            //Assert
+            $this->assertEquals([$test_store, $test_store2], $result);
+        }
+
+        // function test_save()
+        // {
+        //     //Arrange
+        //     $store_name = "Flying Shoes";
+        //     $id = 1;
+        //     $test_store = new Store($store_name, $id);
+        //     $test_store->save();
+        //
+        //     //Act
+        //     $result = Store::getAll();
+        //
+        //     //Assert
+        //     $this->assertEquals($test_store, $result[0]);
+        // }
+        //
+        // function test_deleteAll()
+        // {
+        //     //Arrange
+        //     $store_name = "Flying Shoes";
+        //     $test_store = new Store($store_name);
+        //     $test_store->save();
+        //
+        //     //Act
+        //     Store::deleteAll();
+        //     $result = Store::getAll();
+        //
+        //     //Assert
+        //     $this->assertEquals([], $result);
+        // }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
