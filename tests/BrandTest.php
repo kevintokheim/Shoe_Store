c7586ea45a71dd9b667d20e4b5b6d8a6762ffef3
Kevin Tokheim <katokh89@gmail.com>
2015-08-28 10:41:12 -0700

    add getAll, save and deleteAll methods and tests for Brand class.

--- tests/BrandTest.php
+++ tests/BrandTest.php
@@ -14,10 +14,10 @@
 
     class BrandTest extends PHPUnit_Framework_TestCase
     {
-        // protected function tearDown()
-        // {
-        //     Brand::deleteAll();
-        // }
+        protected function tearDown()
+        {
+            Brand::deleteAll();
+        }
 
         function test_setBrandName()
         {
@@ -58,6 +58,52 @@
 
             //Assert
             $this->assertEquals(1, $result);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $brand_name = "Puma";
+            $test_brand = new Brand($brand_name);
+            $test_brand->save();
+            $brand_name2 = "Nike";
+            $test_brand2 = new Brand($brand_name2);
+            $test_brand2->save();
+
+            //Act
+            $result = Brand::getAll();
+
+            //Assert
+            $this->assertEquals([$test_brand, $test_brand2], $result);
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $brand_name = "Puma";
+            $test_brand = new Brand($brand_name);
+            $test_brand->save();
+
+            //Act
+            $result = Brand::getAll();
+
+            //Assert
+            $this->assertEquals($test_brand, $result[0]);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $brand_name = "Nike";
+            $test_brand = new Brand($brand_name);
+            $test_brand->save();
+
+            //Act
+            Brand::deleteAll();
+            $result = $test_brand->getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
         }
     }
 
