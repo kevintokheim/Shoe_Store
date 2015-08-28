201ea4946a46c11ebc5cb692b4c7fa6c66dfc8b5
Kevin Tokheim <katokh89@gmail.com>
2015-08-28 15:16:15 -0700

    add route for user to add a store to the store.html.twig page.

--- app/app.php
+++ app/app.php
@@ -39,6 +39,12 @@
         return $app['twig']->render("stores.html.twig", array('stores' => $stores, 'brands' => $brands));
     });
 
+    //Adds a store to the page after the user clicks the "add" button
+    $app->post('/store_added', function() use ($app) {
+        $store = new Store($_POST['store_name']);
+        $store->save();
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
 
 
     return $app;
